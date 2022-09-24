42b44d5a4729e3943982174739537606ad877b01
PrakyathKantharaju <prakyathk33@gmail.com>
2022-09-24 18:32:25 -0500

    update to match https://github.com/ikostrikov/dmcgym

--- dmc2gym/__init__.py
+++ dmc2gym/__init__.py
@@ -1,52 +1,34 @@
 import gym
+from dm_control import suite, manipulation
 from gym.envs.registration import register
 
+from dmcgym.env import DMCGYM
 
-def make(
-        domain_name,
-        task_name,
-        seed=1,
-        visualize_reward=True,
-        from_pixels=False,
-        height=84,
-        width=84,
-        camera_id=0,
-        frame_skip=1,
-        episode_length=1000,
-        environment_kwargs=None,
-        time_limit=None,
-        channels_first=True
-):
-    env_id = 'dmc_%s_%s_%s-v1' % (domain_name, task_name, seed)
 
-    if from_pixels:
-        assert not visualize_reward, 'cannot use visualize reward when learning from pixels'
+def create_dm_control_env(domain_name: str, task_name: str) -> gym.Env:
+    env = suite.load(domain_name=domain_name, task_name=task_name)
+    return DMCGYM(env)
 
-    # shorten episode length
-    max_episode_steps = (episode_length + frame_skip - 1) // frame_skip
 
-    if not env_id in gym.envs.registry.env_specs:
-        task_kwargs = {}
-        if seed is not None:
-            task_kwargs['random'] = seed
-        if time_limit is not None:
-            task_kwargs['time_limit'] = time_limit
-        register(
-            id=env_id,
-            entry_point='dmc2gym.wrappers:DMCWrapper',
-            kwargs=dict(
-                domain_name=domain_name,
-                task_name=task_name,
-                task_kwargs=task_kwargs,
-                environment_kwargs=environment_kwargs,
-                visualize_reward=visualize_reward,
-                from_pixels=from_pixels,
-                height=height,
-                width=width,
-                camera_id=camera_id,
-                frame_skip=frame_skip,
-                channels_first=channels_first,
-            ),
-            max_episode_steps=max_episode_steps,
-        )
-    return gym.make(env_id)
+def create_dm_control_env_manipulation(environment_name: str) -> gym.Env:
+    env = manipulation.load(environment_name=environment_name)
+    return DMCGYM(env)
+
+
+create_dm_control_env.metadata = DMCGYM.metadata
+create_dm_control_env_manipulation.metadata = DMCGYM.metadata
+
+for (domain_name, task_name) in suite.ALL_TASKS:
+    register(id=f"{domain_name}-{task_name}-v0",
+             entry_point="dmcgym:create_dm_control_env",
+             max_episode_steps=1000,
+             kwargs=dict(domain_name=domain_name, task_name=task_name))
+
+for environment_name in manipulation.ALL:
+    # only load proprioceptive versions
+    if 'features' in environment_name:
+        env_name = environment_name[:-(1 + len('features'))]
+        register(id=f"{env_name}-v0",
+                 entry_point="dmcgym:create_dm_control_env_manipulation",
+                 max_episode_steps=1000,
+                 kwargs=dict(environment_name=environment_name))
--- setup.py
+++ setup.py
@@ -1,17 +1,45 @@
 import os
-from setuptools import setup
-from setuptools import find_packages
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+
+def read_requirements_file(filename):
+    req_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                 filename)
+    with open(req_file_path) as f:
+        return [line.strip() for line in f]
+
+
+description = ('A gym wrapper for DeepMind Control suite environments.')
+
+install_requires = read_requirements_file('requirements.txt')
 
 setup(
-    name='dmc2gym',
-    version='1.0.0',
-    author='Denis Yarats-edit Prakyath',
-    description=('a gym like wrapper for dm_control'),
-    license='',
-    keywords='gym dm_control openai deepmind',
-    packages=find_packages(),
-    install_requires=[
-        'gym',
-        'dm_control',
+    name='dmcgym',
+    version='0.0.1',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/prakyathkantharaju/dmc2gym',
+    author='Prakyath Kantharaju (Update) from ikostrikov',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
+    keywords='reinforcement, machine, learning, research',
+    packages=find_packages(),
+    install_requires=install_requires,
+    license='MIT',
 )
